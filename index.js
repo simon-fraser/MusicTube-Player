651121b3d1043d59855a611e8eb17db963f16712
Simon Fraser <sfraser@mrpfd.com>
2018-08-07 19:46:58 +0100

    basic OS notifications when using the media keys

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { app, BrowserWindow, globalShortcut } = require('electron');
+const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
+const notifier = require('node-notifier');
 const path = require('path');
 
 let mainWindow;
 
 app.on('ready', () => {
+	// Build Main Window
 	mainWindow = new BrowserWindow({
 		frame: true,
 		icon: path.join(__dirname, 'assets/youtube-music.ico'),
@@ -11,25 +13,76 @@ app.on('ready', () => {
 		height: 700,
 		width: 500,
 	})
-	mainWindow.loadURL('https://music.youtube.com')
+	// Load YouTube
+	mainWindow.loadURL('https://music.youtube.com/')
 
 	// Setup Media Keys
 	registerGlobalShortcuts()
 });
 
+ipcMain.on('notify', function(event, obj) {
+	// Notify
+	notifier.notify({
+		title: `${obj.status} • YouTube Music`,
+		message: `${obj.title}\n${obj.by}`,
+		icon: path.join(__dirname, 'assets/youtube-music.ico'),
+		contentImage: null
+	})
+});
+
 function registerGlobalShortcuts() {
 	// Play/Pause
 	globalShortcut.register('MediaPlayPause', () => {
+		// Trigger Play/Pause
 		mainWindow.webContents.executeJavaScript(`document.getElementsByClassName('play-pause-button')[0].click()`)
+
+		setTimeout(() =>{
+			mainWindow.webContents.executeJavaScript(`
+				var ipcRenderer = require('electron').ipcRenderer;
+				var status = (document.getElementsByClassName('play-pause-button')[0].title.includes('Pla'))? 'Paused' : 'Playing';
+				var value = {
+					status: status,
+					title: document.getElementsByClassName('ytmusic-player-bar title')[0].innerText,
+					by: document.getElementsByClassName('ytmusic-player-bar byline')[0].innerText,
+				}
+				ipcRenderer.send('notify', value);
+			`)
+		}, 500)
 	})
 
 	// Next
 	globalShortcut.register('MediaNextTrack', () => {
+		// Trigger Next
 		mainWindow.webContents.executeJavaScript(`document.getElementsByClassName('next-button')[0].click()`)
+
+		setTimeout(() =>{
+			mainWindow.webContents.executeJavaScript(`
+				var ipcRenderer = require('electron').ipcRenderer;
+				var value = {
+					status: "Now Playing",
+					title: document.getElementsByClassName('ytmusic-player-bar title')[0].innerText,
+					by: document.getElementsByClassName('ytmusic-player-bar byline')[0].innerText,
+				}
+				ipcRenderer.send('notify', value);
+			`)
+		}, 500)
 	})
 
 	// Previous
 	globalShortcut.register('MediaPreviousTrack', () => {
+		// Trigger Previous
 		mainWindow.webContents.executeJavaScript(`document.getElementsByClassName('previous-button')[0].click()`)
+
+		setTimeout(() =>{
+			mainWindow.webContents.executeJavaScript(`
+				var ipcRenderer = require('electron').ipcRenderer;
+				var value = {
+					status: "Now Playing",
+					title: document.getElementsByClassName('ytmusic-player-bar title')[0].innerText,
+					by: document.getElementsByClassName('ytmusic-player-bar byline')[0].innerText,
+				}
+				ipcRenderer.send('notify', value);
+			`)
+		}, 500)
 	})
 }
